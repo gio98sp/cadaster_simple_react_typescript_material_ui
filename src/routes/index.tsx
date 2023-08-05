162e373196da81af5cfecdb39d9578dfc4e45fa7
Giovane Santos <giovane98sp@gmail.com>
2023-08-05 13:24:07 -0300

    feat: add react-router-dom

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,1 +1,11 @@
-export {}
+import { Routes, Route, Navigate } from 'react-router-dom'
+import { Dashboard } from '../pages/dashboard/Dashboard'
+
+export const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="*" element={<Navigate to="/dashboard" />} />
+    </Routes>
+  )
+}
