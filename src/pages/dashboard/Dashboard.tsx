e1deddbfb806b0bcbaa17beb027e9c04cd1737af
Giovane Santos <giovane98sp@gmail.com>
2023-08-05 13:57:57 -0300

    feat: add light theme and provider

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@mui/material";
 
 export const Dashboard = () => {
-
   return (
-    <div>Dashboard</div>
-  )
-}
+    <div>
+      Dashboard
+      <Button variant="contained" color="primary">Teste</Button>
+    </div>
+  );
+};
