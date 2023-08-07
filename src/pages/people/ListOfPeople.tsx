8e9ff29d15a1e4470c50930091e5648d8b7275a4
Giovane Santos <giovane98sp@gmail.com>
2023-08-07 18:52:11 -0300

    feat: add custom hook useDebounce

--- src/pages/people/ListOfPeople.tsx
+++ src/pages/people/ListOfPeople.tsx
@@ -2,25 +2,28 @@ import { useEffect, useMemo } from 'react';
 import { useSearchParams } from 'react-router-dom';
 
 import { Toolbar } from '../../shared/components';
+import { useDebounce } from '../../shared/hooks';
 import { LayoutBaseDePagina } from '../../shared/layouts/LayoutBaseDePagina';
 import { PessoasService } from '../../shared/services/api/pessoas/PessoasService';
 
 export const ListOfPeople = () => {
   const [searchParams, setSearchParams] = useSearchParams();
+  const { debounce } = useDebounce();
 
   const busca = useMemo(() => {
     return searchParams.get('busca') || '';
   }, [searchParams]);
 
   useEffect(() => {
-    PessoasService.getAll(1, busca)
-      .then((result) => {
+    debounce(() => {
+      PessoasService.getAll(1, busca).then((result) => {
         if (result instanceof Error) {
           alert(result.message);
         } else {
           console.log(result);
         }
       });
+    });
   }, [busca]);
 
   return (
