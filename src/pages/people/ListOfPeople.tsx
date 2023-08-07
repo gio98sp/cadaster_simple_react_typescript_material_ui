65a1529851f1567f763a9a54b5be2cd86c614b18
Giovane Santos <giovane98sp@gmail.com>
2023-08-07 19:36:49 -0300

    feat: add table for list people

--- src/pages/people/ListOfPeople.tsx
+++ src/pages/people/ListOfPeople.tsx
@@ -1,26 +1,47 @@
-import { useEffect, useMemo } from 'react';
+import {
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from '@mui/material';
+import { useEffect, useMemo, useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
 
 import { Toolbar } from '../../shared/components';
 import { useDebounce } from '../../shared/hooks';
 import { LayoutBaseDePagina } from '../../shared/layouts/LayoutBaseDePagina';
-import { PessoasService } from '../../shared/services/api/pessoas/PessoasService';
+import {
+  IListagemPessoa,
+  PessoasService,
+} from '../../shared/services/api/pessoas/PessoasService';
 
 export const ListOfPeople = () => {
   const [searchParams, setSearchParams] = useSearchParams();
   const { debounce } = useDebounce();
+
+  const [rows, setRows] = useState<IListagemPessoa[]>([]);
+  const [totalCount, settotalCount] = useState(0);
+  const [isLoading, setIsLoading] = useState(true);
 
   const busca = useMemo(() => {
     return searchParams.get('busca') || '';
   }, [searchParams]);
 
   useEffect(() => {
+    setIsLoading(true);
+
     debounce(() => {
       PessoasService.getAll(1, busca).then((result) => {
+        setIsLoading(false);
         if (result instanceof Error) {
           alert(result.message);
         } else {
           console.log(result);
+          settotalCount(result.totalCount);
+          setRows(result.data);
         }
       });
     });
@@ -37,6 +58,27 @@ export const ListOfPeople = () => {
           changeSearchText={(text) => setSearchParams({ busca: text }, { replace: true })}
         />
       }
-    ></LayoutBaseDePagina>
+    >
+      <TableContainer component={Paper} variant={'outlined'} sx={{ m: 1, width: 'auto' }}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Ações</TableCell>
+              <TableCell>Nome Completo</TableCell>
+              <TableCell>Email</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map((row) => (
+              <TableRow key={row.id}>
+                <TableCell></TableCell>
+                <TableCell>{row.nomeCompleto}</TableCell>
+                <TableCell>{row.email}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </LayoutBaseDePagina>
   );
 };
