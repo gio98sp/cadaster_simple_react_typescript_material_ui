4a1e687d9373f50f0eaeee4dfdedcef1037a9e69
Giovane Santos <giovane98sp@gmail.com>
2023-08-10 17:19:21 -0300

    update urlBase and add data

--- src/shared/environment/index.ts
+++ src/shared/environment/index.ts
@@ -19,5 +19,5 @@ export const Environment = {
   /**
    * Url base de consulta de dados dessa aplicação
    */
-  URL_BASE: 'https://json-server-for-cadaster-people-and.onrender.com'
+  URL_BASE: 'http://localhost:3333'
 }
